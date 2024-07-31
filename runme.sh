3d0e82ff93788d82709919dc6f60e56c10a33757
Alex Lou <alexander.loutsenko2@gmail.com>
2024-07-31 19:16:08 +0300

    small output formatting changes

--- runme.sh
+++ runme.sh
@@ -15,6 +15,7 @@ echo "--== This script will insert your mnemonic and set your mnemonic insertion
 echo ' '
 
 echo '# 1) populate some variables to be used by the script'
+echo ' '
 echo '## 1.1) enter your mnemonic here:' 
 
 read mnemonic
@@ -29,20 +30,21 @@ read nodename
 json_node_name='{"nodename":"'$nodename'",'
 json_next_key='"mnemonicInserted":true,'
 echo ' '
-
+echo "-= step #1 is DONE =-"
 
 
 echo '# 2) bash is expected to change the directory to .humanode/workspaces/default folder'
 
 cd $HOME/.humanode/workspaces/default
 echo ' '
-
+echo "-= step #2 is DONE =-"
 
 
 echo '# 3) bash is expected to insert mnemonic by executing the following command'
 
 ./humanode-peer key insert  --key-type kbai --scheme sr25519 --suri "$mnemonic" --base-path substrate-data --chain chainspec.json
 echo ' '
+echo "-= step #3 is DONE =-"
 
 
 
@@ -52,6 +54,7 @@ echo "--== the kbai key just inserted looks like the following:"
 ls_output=$(ls $HOME/.humanode/workspaces/default/substrate-data/chains/humanode_mainnet/keystore)
 printf %s "$ls_output"
 echo ' ' 
+echo "-= step #4 is DONE =-"
 
 
 
@@ -59,6 +62,7 @@ echo '# 5) bash is expected to delete existing workspace.json file'
 
 rm workspace.json
 echo ' '
+echo "-= step #5 is DONE =-"
 
 
 
@@ -66,6 +70,7 @@ echo '# 6) bash is expected to create a new JSON file instead'
 
 echo ${json_node_name}${json_next_key}'"rpcUrlMode":{"type":"ngrok-auto-detect"},"isValidator":false,"tunnelType":"humanode","manualTunnelRuntime":false,"bioauthEnroll":false,"chainspecPath":"chainspec.json","executablePath":"humanode-peer","humanodeWebsocketTunnelClientPath":"humanode-websocket-tunnel","manifestUrl":"https://chainspec.mainnet.stages.humanode.io/latest/manifest.json","ngrokPath":"ngrok-wrapper","executableInfo":{"platform":"Linux","arch":"x86_64","displayName":"Humanode Mainnet"},"substrateAddress":""}' > workspace.json
 echo ' '
+echo "-= step #6 is DONE =-"
 
 
 
